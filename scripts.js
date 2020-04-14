74d144d80936130f59f32f58c9f02bce64c6783d
thevrajshah <42384591+thevrajshah@users.noreply.github.com>
2020-04-14 14:48:21 +0530

    javascript added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
-var home = document.getElementById(home);
-var about = document.getElementById(about);
-var contact = document.getElementById(contact);
-
-function display{
-    
+function showHome() {
+    document.getElementById('home').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+function showAbout() {
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+}
+function showContact() {
+    document.getElementById('contact').style.display = 'block';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
 }
