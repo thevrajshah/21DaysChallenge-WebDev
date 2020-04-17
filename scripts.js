69d2f661de3651a990101a2ccc43bdd28e8eaf6b
thevrajshah <42384591+thevrajshah@users.noreply.github.com>
2020-04-18 01:21:35 +0530

    logo update

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
 var home = document.getElementById('home');
 var about = document.getElementById('about');
 var contact = document.getElementById('contact');
+var logo = document.getElementById('logo');
 
 function showHome() {
     home.style.display = 'block';
     about.style.display = 'none';
     contact.style.display = 'none';
+    logo.style.visibility = 'hidden';
 }
 function showAbout() {
     about.style.display = 'block';
     home.style.display = 'none';
     contact.style.display = 'none';
+    logo.style.visibility = 'visible';
+
 }
 function showContact() {
     contact.style.display = 'block';
     home.style.display = 'none';
     about.style.display = 'none';
+    logo.style.visibility = 'visible';
 }
-if (home.style.display = 'block') {
-    navBtn[1].classList.add("active");
-}
-console.log(navBtn[1])
